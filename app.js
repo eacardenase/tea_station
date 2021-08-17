3794a3413297080f4838e5f775dbeb2fe7fe6740
eacardenase <eacardenase@unal.edu.co>
2021-08-16 21:17:37 -0500

    footer html

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 // show nav
 navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
+    navbar.classList.add("showNav");
 });
 // close nav
 navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
+    navbar.classList.remove("showNav");
 });
 //setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const date = (document.getElementById("date").innerHTML =
+    new Date().getFullYear());
